eb888f612d3a7f1ac2391e6a9040f059f028fa88
qiaoronggui <903111082@qq.com>
2024-02-05 17:40:07 +0800

    feat: 增加代码配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,21 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    // 当前项目不支持 ts
+    // '@vue/eslint-config-typescript/recommended'
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+  env: {
+    'vue/setup-compiler-macros': true
+  },
+  rules: {
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ["index"]
+    }],
+    "vue/no-setup-props-destructure": ["off"],
+    // 支持对 defineProps 解构
+    "vue/no-mutating-props": ["off"]
   }
 }
