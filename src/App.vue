3f19d62fff60b807667197f2eaeffecd7e9920d0
qiaoronggui <903111082@qq.com>
2024-02-06 09:36:56 +0800

    feat: 添加 ant-design-vue 组件库

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
+<script setup>
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+
+// 设置主题色
+const theme = {
+  token: {
+    colorPrimary: '#6226EE',
+  },
+}
+dayjs.locale('zh-cn')
+</script>
+
 <template>
-  <div class="app">
-    <!-- 存放路由的容器 必须要有这个东西- 没有容器, 就没有办法展示组件 -->
-    <router-view></router-view>
-  </div>
+  <a-config-provider :theme="theme" :locale="zhCN">
+    <div class="app">
+      <a-space wrap>
+        <a-button type="primary">Primary Button</a-button>
+        <a-button>Default Button</a-button>
+        <a-button type="dashed">Dashed Button</a-button>
+        <a-button type="text">Text Button</a-button>
+        <a-button type="link">Link Button</a-button>
+      </a-space>
+      <router-view></router-view>
+    </div>
+  </a-config-provider>
 </template>
 <style>
 .app {
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 自动导入vue中hook reactive ref等
+import AutoImport from "unplugin-auto-import/vite"
+//自动导入 ui组件
+import Components from 'unplugin-vue-components/vite';
+//ant-design-vue
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      // 组件中不用再导入ref，reactive等
+      imports: ['vue', 'vue-router'],
+      //ant-design-vue
+      resolvers: [AntDesignVueResolver()]
+    }),
+    Components({
+      resolvers: [AntDesignVueResolver({ importStyle: false, resolveIcons: true })],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
 })
